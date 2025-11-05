85ef470148c95a82ae5d3ceaa6dba1d4531aebc1
Bhavani <bhavani@testpress.in>
2025-11-05 12:57:36 +0000

    Added Food model

--- restaurants/models.py
+++ restaurants/models.py
@@ -74,5 +74,24 @@ class Visited(models.Model):
 
     def __str__(self):
         return f"{self.user.username} visited {self.restaurant.name}"
+    
+
+
+class Food(models.Model):
+    VEG_CHOICES = [
+        ('veg', 'Vegetarian'),
+        ('nonveg', 'Non-Vegetarian'),
+        ('vegan', 'Vegan'),
+    ]
+
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='menu')
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_veg = models.CharField(max_length=10, choices=VEG_CHOICES)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.restaurant.name}"
+
 
 
