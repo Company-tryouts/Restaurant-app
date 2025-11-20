1704c9b3d8a53587ce7f4931a71e687e73d35ab3
bhavani-b03 <bhavani@testpress.in>
2025-11-20 15:25:52 +0530

    feat : Add menu page for each restaurant
    - Created the menu page for each restaurant where its dishes are listed in one place.
    - Users can find the name and price of the dishes in this menu page.

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Restaurant
+from django.shortcuts import render, get_object_or_404
+from .models import Restaurant, Food, Cuisine
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
@@ -23,3 +23,17 @@ class RestaurantDetailView(DetailView):
 
     def get_queryset(self):
         return super().get_queryset().prefetch_related('images', 'cuisines')
+    
+class FoodListView(ListView):
+    model = Food
+    template_name = "foods/list.html"
+    context_object_name = "foods"
+
+    def get_queryset(self):
+        self.restaurant = get_object_or_404(Restaurant, pk=self.kwargs['restaurant_id'])
+        return Food.objects.filter(restaurant=self.restaurant).prefetch_related('cuisines')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['restaurant'] = self.restaurant
+        return context
