8774ca12a64029d3619f457d1fe68a79d7ab1a6c
Bhavani <bhavani@testpress.in>
2025-12-01 12:18:21 +0000

    feat:add sorting by price feature to restaurants

--- restaurants/filters.py
+++ restaurants/filters.py
@@ -21,7 +21,21 @@ class RestaurantFilter(django_filters.FilterSet):
         choices=[(n, n) for n in range(1, 6)],  # 1 to 5 stars
         lookup_expr="exact"
     )
+    
+    sort_by = django_filters.ChoiceFilter(
+        method="filter_by_sort",
+        choices=[
+            ('price_low', 'Low → High'),
+            ('price_high', 'High → Low'),
+        ])
 
     class Meta:
         model = Restaurant
         fields = ['cost_for_two_min', 'cost_for_two_max', 'diet_type', 'cuisines', 'rating']
+
+    def filter_by_sort(self, queryset, name, value):
+        if value == "price_low":
+            return queryset.order_by("cost_for_two")
+        if value == "price_high":
+            return queryset.order_by("-cost_for_two")
+        return queryset
--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,8 +1,9 @@
 from django.urls import reverse
 from django.test import TestCase
-from .models import Bookmark, Visited, Review
+from .models import Bookmark, Visited, Review, Restaurant
 from restaurants.test_restaurants.mixins import RestaurantTestSetupMixin
 from django.contrib.auth.models import User
+from restaurants.filters import RestaurantFilter
 
 class TestRestaurantListView(RestaurantTestSetupMixin, TestCase):
     def test_list_page_should_load_restaurants(self):
@@ -88,6 +89,17 @@ class TestRestaurantListView(RestaurantTestSetupMixin, TestCase):
         for r in included:
             self.assertContains(response, r.name)
 
+    def test_sort_price_low_to_high(self):
+        data = {"sort_by": "price_low"}
+        qs = RestaurantFilter(data=data, queryset=Restaurant.objects.all()).qs
+
+        assert list(qs.values_list("cost_for_two", flat=True)) == [200, 400, 500, 1000]
+
+    def test_sort_price_high_to_low(self):
+        data = {"sort_by": "price_high"}
+        qs = RestaurantFilter(data=data, queryset=Restaurant.objects.all()).qs
+
+        assert list(qs.values_list("cost_for_two", flat=True)) == [1000, 500, 400, 200]
 
 class TestRestaurantDetailView(RestaurantTestSetupMixin, TestCase):
     def test_detail_page_should_display_correct_restaurant(self):
