4db9a70b855c4664bf5e7e61b9e7865aa81a81ca
Bhavani <bhavani@testpress.in>
2025-12-03 11:16:58 +0000

    refactor:use the diet type choices from the database

--- restaurants/filters.py
+++ restaurants/filters.py
@@ -1,19 +1,13 @@
 import django_filters
 from django import forms
 from .models import Restaurant, DietType
-
-CUSTOM_DIET_CHOICES = [
-    (1, "Veg"),       
-    (2, "Non Veg"),
-    (3, "Vegan")   
-]
 class RestaurantFilter(django_filters.FilterSet):
 
     cost_for_two_min = django_filters.NumberFilter(field_name="cost_for_two", lookup_expr="gte")
     cost_for_two_max = django_filters.NumberFilter(field_name="cost_for_two", lookup_expr="lte")
 
     diet_type = django_filters.MultipleChoiceFilter(
-        choices=CUSTOM_DIET_CHOICES,
+        choices=DietType.choices,
         widget=forms.CheckboxSelectMultiple
     )
     class Meta:
